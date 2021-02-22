b26a409218767def645b882b39c79067af5921bf
oopanpan <oopanpan@gmail.com>
2021-02-21 23:38:10 -0800

    commit tree d24284606e4c2a6f007923974c1052b7c45f0d48

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    let result = [];
+    for(let i = 0; i < name.length; i++) {
+        result.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return result;
+}
+
+function countDown(){
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter);
+        counter-=1
+    }
+}
